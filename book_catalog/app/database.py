7c598aeee4e9433cff9f90adf97d186398590fe2
Corie Rhodes <corierhodes@yahoo.com.au>
2024-09-26 16:34:57 +1000

    Finished project config, all tested as working.

--- book_catalog/app/database.py
+++ book_catalog/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://inventory_knse_user:W6PKDpgHHJ9VUzHmkMqJuUEnc0mkqfge@dpg-crmf2iq3esus73fs09r0-a.oregon-postgres.render.com/inventory_knse" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://part03_user:qjwuMVamfmrDdtBe89v2RXpKSVtwn1aI@dpg-crqejpu8ii6s73ctectg-a.oregon-postgres.render.com/part03" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://part03_user:qjwuMVamfmrDdtBe89v2RXpKSVtwn1aI@dpg-crqejpu8ii6s73ctectg-a.oregon-postgres.render.com/part03')
